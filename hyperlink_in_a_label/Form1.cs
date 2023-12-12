a1772ced8cdabe7297040478b8c3454473440d59
jasaga2401 <jasaga@protonmail.com>
2023-12-12 23:41:49 +0000

    Add project files.

---
+++ hyperlink_in_a_label/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hyperlink_in_a_label
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LinkLabel linkLabel = new LinkLabel();
+            linkLabel.Text = "Visit our website";
+            linkLabel.Location = new System.Drawing.Point(20, 20);
+            linkLabel.AutoSize = true;
+            linkLabel.LinkClicked += new LinkLabelLinkClickedEventHandler(LinkLabel_LinkClicked);
+
+            this.Controls.Add(linkLabel);
+        }
+
+        private void LinkLabel_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://www.bbc.co.uk");
+        }
+    }
+}
